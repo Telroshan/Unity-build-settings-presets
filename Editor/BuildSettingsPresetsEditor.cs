7a70ebb024c2373da7f62dc704872b6f0254e941
Telroshan <vincent.thebaud@ynov.com>
2019-09-05 20:10:47 +0200

    New preset saving

--- Editor/BuildSettingsPresetsEditor.cs
+++ Editor/BuildSettingsPresetsEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace Editor
@@ -6,6 +7,8 @@ namespace Editor
     [InitializeOnLoad]
     public class BuildSettingsPresetsEditor
     {
+        private const string ROOT_FOLDER = "Assets";
+
         static BuildSettingsPresetsEditor()
         {
             Debug.Log("Initialization");
@@ -15,6 +18,18 @@ namespace Editor
         private static void AddPreset()
         {
             Debug.Log("New preset !");
+            BuildSettingsPreset preset = BuildSettingsPreset.FromCurrentSettings();
+            Debug.Log(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(preset)));
+            string dirname =
+                Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(preset))) ??
+                ROOT_FOLDER;
+            if (dirname != ROOT_FOLDER)
+            {
+                dirname = Path.Combine(Directory.GetParent(dirname).ToString(), "Presets");
+            }
+            string path = Path.Combine(dirname, "NewPreset.asset");
+            
+            AssetDatabase.CreateAsset(preset, path);
         }
     }
 }
