98d5e1dbb7b8f75361aeb14ee8fe2ccec8e53679
Telroshan <vincent.thebaud@ynov.com>
2019-09-06 13:16:34 +0200

    BuildSettingsPresetEditor (just an "Import" button on top)

---
+++ Editor/BuildSettingsPresetEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(BuildSettingsPreset))]
+    public class BuildSettingsPresetEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Import"))
+            {
+                AssetDatabase.TryGetGUIDAndLocalFileIdentifier(target, out string guid, out long localId);
+                BuildSettingsPresetsManager.ImportPreset(guid);
+            }
+
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            DrawDefaultInspector();
+        }
+    }
+}
